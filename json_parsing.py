66819ea84e4639bd7ca2ff20c9b13a390a45ef9e
Amatlakhova <alina.matlakhova@gmail.com>
2021-08-23 06:26:23 +0300

    Long redirect

--- json_parsing.py
+++ json_parsing.py
@@ -1,10 +1,13 @@
 import requests
 
-response = requests.post("https://playground.learnqa.ru/api/get_301", allow_redirects=True)
+response = requests.get("https://playground.learnqa.ru/api/long_redirect", allow_redirects=True)
+
 first_redirect = response.history[0]
-second_redirect = response
+second_redirect = response.history[1]
+third_redirect = response.history[2]
 
 print(first_redirect.url)
 print(second_redirect.url)
+print(third_redirect.url)
 
-
+print(response.url)
