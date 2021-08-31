8c5f94ccb25b3fe5720156e48b51deeaa0f6030f
Amatlakhova <alina.matlakhova@gmail.com>
2021-08-31 21:00:23 +0300

    first pytest: parametrized hello api method

--- test_first_api.py
+++ test_first_api.py
@@ -1,9 +1,16 @@
+import pytest
 import requests
 
 class TestFirstAPI:
-    def test_hello_call(self):
+    names = [
+        ("Vitalii"),
+        ("Arseniy"),
+        ("")
+    ]
+
+    @pytest.mark.parametrize('name', names)
+    def test_hello_call(self, name):
         url = "https://playground.learnqa.ru/api/hello"
-        name = 'Alina'
         data = {'name': name}
 
         response = requests.get(url, params= data)
@@ -13,6 +20,9 @@ class TestFirstAPI:
         response_dict = response.json()
         assert "answer" in response_dict, "There is no field 'answer' in response"
 
-        expected_response_text = f"Hello, {name}"
+        if len(name) == 0:
+            expected_response_text = "Hello, someone"
+        else:
+            expected_response_text = f"Hello, {name}"
         actual_response_text = response_dict["answer"]
         assert actual_response_text == expected_response_text, "Actual response text is not correct"
