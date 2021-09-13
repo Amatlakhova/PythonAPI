7605aa43a02be6f206d2f1aadb1c46215f5bb220
Amatlakhova <alina.matlakhova@gmail.com>
2021-09-13 17:45:44 +0300

    Logs

--- lib/my_requests.py
+++ lib/my_requests.py
@@ -1,5 +1,7 @@
 import requests
 
+from lib.logger import Logger
+
 
 class MyRequests():
     @staticmethod
@@ -28,6 +30,8 @@ class MyRequests():
         if cookies is None:
             cookies = {}
 
+        Logger._add_request(url, data, headers, cookies, method)
+
         if method == 'GET':
             response = requests.get(url, params=data, headers=headers, cookies=cookies)
         elif method == 'POST':
@@ -38,6 +42,8 @@ class MyRequests():
             response = requests.delete(url, data=data, headers=headers, cookies=cookies)
         else:
             raise Exception(f"Bad HTTP method {method} was received")
+
+        Logger._add_response(response)
 
         return response
 
--- tests/test_phrase_length.py
+++
@@ -1,9 +0,0 @@
-class TestPhraseLength:
-    def test_phrase_length(self):
-        text = input("Please enter text less than 15 symbols: ")
-        print("You entered: " + text)
-
-        expected_length = 15
-        actual_length = len(text)
-
-        assert actual_length <= expected_length, f"Length of entered text is > 15 symbols. Actual length is {actual_length}"
